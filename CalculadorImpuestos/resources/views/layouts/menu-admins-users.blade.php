98b05460d7e6c4c29b75840aac7f4e5d4a3667d3
LeoNardoz333 <leovel330@gmail.com>
2025-03-02 20:18:31 -0600

    Design's responsiveness working correctly.

--- CalculadorImpuestos/resources/views/layouts/menu-admins-users.blade.php
+++ CalculadorImpuestos/resources/views/layouts/menu-admins-users.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color:rgb(60, 206, 155);">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}">Calculador de Impuestos - SAT</a>
+        <a class="navbar-brand text-wrap" href="{{ route('home') }}">Calculador de Impuestos - SAT</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
