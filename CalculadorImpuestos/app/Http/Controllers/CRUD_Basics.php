50fc922cb550c3691465850f086cdcf7546ef9f1
LeoNardoz333 <leovel330@gmail.com>
2025-10-05 17:06:08 -0600

    Controller skeleton for CURD_Basics created

--- CalculadorImpuestos/app/Http/Controllers/CRUD_Basics.php
+++ CalculadorImpuestos/app/Http/Controllers/CRUD_Basics.php
@@ -3,8 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CRUD_Basics extends Controller
 {
-    //
+    ## - - - - - - - - - - - - - - - -  get methods - - - - - - - - - - - - - - -  ##
+    public function getDocument($filter)
+    {
+
+    }
+
+    public function getDocuments()
+    {
+
+    }
+
+    public function getDocumentsByFilter($filter)
+    {
+
+    }
+
+    public function getDocumentsByFilters(array $filters)
+    {
+
+    }
+    
+    ## - - - - - - - - - - - - - - - -  delete methods - - - - - - - - - - - - - - -  ##    
+    
+    public function deleteDocument($filter)
+    {
+
+    }
+
+    public function deleteAllDocuments()
+    {
+
+    }
+
+    public function deleteDocumentByFilters(array $filters)
+    {
+
+    }
+
+    ## - - - - - - - - - - - - - - - -  update methods - - - - - - - - - - - - - - -  ##    
+    public function updateDocument($filter)
+    {
+
+    }
+
+    public function updateDocuments()
+    {
+
+    }
+
+    public function updateDocumentByFilters(array $filters)
+    {
+
+    }
 }
