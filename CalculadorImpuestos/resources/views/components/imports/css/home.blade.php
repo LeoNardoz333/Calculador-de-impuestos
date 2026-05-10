0a77352784976ddbfb99d67607a53d04b076904e
LeoNardoz333 <leo.vel330@gmail.com>
2026-05-10 14:33:05 -0600

    improve: project resources directory structure

--- CalculadorImpuestos/resources/views/components/imports/css/home.blade.php
+++
@@ -1,5 +0,0 @@
-@vite(['resources/css/elements/Botones.css', 'resources/css/elements/Fuentes.css',
- 'resources/css/elements/Iconos.css', 'resources/css/elements/Texto.css',
-  'resources/js/app.js'])
-  
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
