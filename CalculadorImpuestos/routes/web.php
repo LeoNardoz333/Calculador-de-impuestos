de5aabb93e245087b86bf0cc6de60e8ae137b060
LeoNardoz333 <leo.vel330@gmail.com>
2026-05-24 15:04:00 -0600

    refactor(routes): restructure authentication and dashboards routes using groups

--- CalculadorImpuestos/routes/web.php
+++ CalculadorImpuestos/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use GuzzleHttp\Middleware;
 
 /*
@@ -15,23 +16,44 @@ use GuzzleHttp\Middleware;
 |
 */
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Home - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-Route::get('/', function () { return view('/login/login-usuarios'); })->name('home');
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - home - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Route::get('/', function () {
+    return redirect()->route('auth.users.login');
+})->name('home');
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - usuarios - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-Route::get('/login/login-usuarios', function () { return view('/login/login-usuarios'); })
-->name('V_login-usuarios');
-Route::post('/login/login-usuarios', [UserController::class, 'validarLogin'])->name('login.validate');
-Route::get('menu-usuarios', function(){ return view('menu-usuarios'); })->middleware('auth')->name('v_menu-usuarios');
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - auth - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Route::prefix('auth')->name('auth.')->group(function () {
+    #Users authentication
+    Route::get('users', function () {
+        return view('auth.users.login');
+    })->name('users.login');
+
+    Route::post('users', [AuthController::class, 'store'])->name('login.validate');
+    Route::post('users/login', [UserController::class, 'attempt'])->name('user.attempt');
+
+
+    Route::get('admins', function () {
+        return view('admins.index');
+    })->name('admins.login');
+
+    #Sign up
+    Route::get('sign-up', function () {
+        return view('auth.register');
+    })->name('sign-up');
+
+    Route::post('sign-up', [UserController::class, 'store'])->name('register.store');
+});
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - users - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Route::middleware(['auth'])->prefix("users")->name("users.")->group(function () {
+    Route::get('dashboard', function () {
+        return view('users.home.index');
+    })->name('dashboard');
+});
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - admins - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-#login
-Route::get('/login/login-admins', function() { return view('/login/login-admins'); })
-->name('V_login-admins');
-#Menu
-Route::get('/menu-admins', function() {return view('/admins/menu-admins'); })->name('v_menu-admins');
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - registro de usuarios - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-Route::get('login/sign-up', function() {return view('/login/sign-up'); })->name('v_sign-up');
-Route::post('/login/sign-up', [UserController::class, 'store'])->name('users.store');
-Route::post('/login/login-usuarios',[UserController::class, 'validarLogin'])->name('user.login');
+Route::prefix('admins')->name('admins.')->group(function () {
+    Route::get('dashboard', function () {
+        return view('admins.home.index');
+    })->name('dashboard');
+});
