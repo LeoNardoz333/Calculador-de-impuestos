c5d47e47583dc058139a87cb464e00bc87bf3486
LeoNardoz333 <leo.vel330@gmail.com>
2026-06-19 19:45:17 -0600

    chore: create auth controller

--- web/app/Http/Controllers/AuthController.php
+++ web/app/Http/Controllers/AuthController.php
@@ -6,62 +6,69 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\RateLimiter;
-use App\Services\MongoService;
+use App\Models\User;
+use App\Enums\UserRole;
+use App\Http\Requests\RegisterUserRequest;
 
 class AuthController extends Controller
 {
-    /**
-     * Servicio de MongoDB inyectado.
-     *
-     * @var MongoService
-     */
-    protected MongoService $mongoService;
-    
-    public function __construct(MongoService $mongoService)
+    public function register(RegisterUserRequest $request)
     {
-        $this->mongoService = $mongoService;
-    }
-    
-    public function attempt(Request $request)
-    {
-        #dd($request->all());
-        $validated = $request->validate([
-            'usuario' => 'required|string',
-            'password' => 'required|string',
+        $user = User::create([
+            ...$request->validated(),
+            'role' => UserRole::TAXPAYER,
         ]);
 
-        $key = 'login-attemps-' . $request->ip();
+        return redirect()
+            ->route('auth.users.login')
+            ->with('success', 'Usuario registrado exitosamente. Por favor, inicie sesión.');
+    }
+
+    public function login(Request $request)
+    {
+        $routes = [
+            UserRole::ADMIN => 'admins.dashboard',
+            UserRole::TAXPAYER => 'users.dashboard',
+            UserRole::ACCOUNTANT => 'accountants.dashboard',
+        ];
+
+        $validated = $request->validate([
+            'login' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $key = 'login-attempts-' . $request->ip() . '-' . $validated['login'];
+
         if (RateLimiter::tooManyAttempts($key, 5)) {
             throw ValidationException::withMessages([
-                'password' => ['Demasiados intentos. Inténtalo de nuevo en unos minutos.'],
+                'login' => ['Demasiados intentos. Inténtalo de nuevo en unos minutos.'],
             ]);
         }
 
+        $field = filter_var($validated['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
         if (Auth::attempt([
-            'usuario' => $validated['usuario'],
+            $field => $validated['login'],
             'password' => $validated['password']
-        ], $request->filled('remember'))) {
+        ], $request->boolean('remember'))) {
+
             RateLimiter::clear($key);
-            $request->session()->regenerate();                                  #document, field, filter
-            $usuario = $this->mongoService->getDocument("users", "usuario", $validated['usuario']);
-            if ($usuario->permisos == 'admin' && $request->permisos == 'admin')
-                return redirect()->intended(route('v_menu-admins'));
-            else if ($usuario->permisos == 'usuario' && $request->permisos == 'user')
-                return redirect()->intended(route('v_menu-usuarios'));
-            else if ($usuario->permisos == 'admin' && $request->permisos == 'user') {
-                Auth::logout();
-                return back()->withErrors(['usuario' => 'Las credenciales son incorrectas.']);
-            } else if ($usuario->permisos == 'usuario' && $request->permisos == 'admin') {
-                Auth::logout();
-                return back()->withErrors(['usuario' => 'Este usuario no cuenta con permisos de administrador.']);
-            } else {
-                Auth::logout();
-                return back()->withErrors(['usuario' => 'Las credenciales son incorrectas.']);
-            }
+            $request->session()->regenerate();
+            $user = Auth::user();
+
+            return match ($user->role) {
+                UserRole::ADMIN => redirect()->intended('admins/dashboard'),
+                UserRole::TAXPAYER => redirect()->route('users.dashboard'),
+                UserRole::ACCOUNTANT => redirect()->route('accountants.dashboard'),
+                
+                default => tap(Auth::logout(), fn() =>
+                    back()->withErrors(['login' => 'Rol inválido.'])
+                ),
+            };
         }
 
         RateLimiter::hit($key, 60);
-        return back()->withErrors(['usuario' => 'Las credenciales son incorrectas.']);
+        return back()->withErrors(['login' => 'Las credenciales son incorrectas.']);
     }
 
     public function logout(Request $request)
